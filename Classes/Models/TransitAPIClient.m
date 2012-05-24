22eda5b71092784e5407a273869cab17f15f7212
Andy Atkinson <andy.atkinson@livingsocial.com>
2012-05-24 19:04:43 -0400

    Whoops! left server as local. Bumped to 0.4

--- Classes/Models/TransitAPIClient.m
+++ Classes/Models/TransitAPIClient.m
@@ -12,8 +12,8 @@
 
 NSString * const kTransitAPIClientID = @"94adbe66804dbf92e7290de08334d5d0";
 
-//NSString * const kTransitAPIBaseURLString = @"http://api.beetlefight.com";
-NSString * const kTransitAPIBaseURLString = @"http://localhost:3000";
+NSString * const kTransitAPIBaseURLString = @"http://api.beetlefight.com";
+//NSString * const kTransitAPIBaseURLString = @"http://localhost:3000";
 
 @implementation TransitAPIClient
 
