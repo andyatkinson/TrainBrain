80250d650fd33537461fafa1aa06ae93ef20939d
Andy Atkinson <andy.atkinson@livingsocial.com>
2012-06-19 23:46:59 -0400

    Increase the size of the title area on the route cell

--- Classes/Views/RouteCell.m
+++ Classes/Views/RouteCell.m
@@ -65,7 +65,7 @@
     frame = CGRectMake(boundsX + 10, 15, 27, 28);
     self.icon.frame = frame;
     
-		frame = CGRectMake(boundsX + 46, 10, 170, 20);
+		frame = CGRectMake(boundsX + 46, 10, 200, 20);
 		self.title.frame = frame;
     
     frame = CGRectMake(boundsX + 46, 28, 200, 20);
